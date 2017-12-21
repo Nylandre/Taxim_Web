4ad1be005a1b0a113f1605302b27c005c759e06e
Taha Aksu <tahaaksu01@gmail.com>
2017-12-22 01:36:51 +0300

    Trip complaint page done, gridview + filtering added.

--- Taxim/App_Code/SqlConClass.cs
+++ Taxim/App_Code/SqlConClass.cs
@@ -31,4 +31,29 @@ public class SqlConClass
             }
         }
     }
+    public string FilterComplaints(int? Issue_id,string complaint,string complainerEmail,int? trip_ID)
+    {
+        using (SqlConnection con = new SqlConnection("Data Source=hamstertainment.com;Initial Catalog=Taxim;User Id=taxim_dbo ;Password=tX_2018!"))
+        {
+            using (SqlCommand cmd = new SqlCommand("sp_tblIssue_Filter @Issue_id,@complaint,@complainerEmail,@trip_ID"))
+            {
+                cmd.Parameters.AddWithValue("@Issue_id", Issue_id);
+                cmd.Parameters.AddWithValue("@complaint", complaint);
+                cmd.Parameters.AddWithValue("@complainerEmail", complainerEmail);
+                cmd.Parameters.AddWithValue("@trip_ID", trip_ID);
+                cmd.Connection = con;
+                con.Open();
+                string result = "";
+                using (SqlDataReader dr = cmd.ExecuteReader())
+                {
+                    while (dr.Read())
+                    {
+                        result = dr[0].ToString();
+                    }
+                }
+                con.Close();
+                return result;
+            }
+        }
+    }
 }
