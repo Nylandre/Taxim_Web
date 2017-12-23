7a605a49308238f8c0fe66c4781322da40f8721b
onurkulak <kulaksizoglu.onur@gmail.com>
2017-12-23 08:24:46 +0300

    created gui for create_trip page, back end is not programmed

--- Taxim/Create_Trip.aspx.cs
+++ Taxim/Create_Trip.aspx.cs
@@ -11,4 +11,38 @@ public partial class Create_Trip : System.Web.UI.Page
     {
 
     }
+    
+
+    protected void CheckBox1_CheckedChanged(object sender, EventArgs e)
+    {
+        if (CheckBox1.Checked)
+        {
+            CheckBox2.Checked = true;
+            CheckBox2.Enabled = false;
+        }
+        else
+            CheckBox2.Enabled = true;
+    }
+
+
+    protected void TextBox3_TextChanged(object sender, EventArgs e)
+    {
+
+    }
+
+    protected void TextBox2_TextChanged(object sender, EventArgs e)
+    {
+
+    }
+
+    protected void TextBox4_TextChanged(object sender, EventArgs e)
+    {
+
+    }
+
+    protected void RadioButtonList1_SelectedIndexChanged(object sender, EventArgs e)
+    {
+        TextBox4.Enabled = !RadioButtonList1.SelectedValue.Equals(RadioButtonList1.Items[0]);
+        //if user chooses to order taxi now, textbox gets disabled
+    }
 }
