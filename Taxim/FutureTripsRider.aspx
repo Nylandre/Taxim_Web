808be8d99da0d0626d28c82134b0aac1322b43ce
Taha Aksu <tahaaksu01@gmail.com>
2017-12-24 00:58:37 +0300

    RAteDriver and FutureTripsRider added but not tested yet

---
+++ Taxim/FutureTripsRider.aspx
@@ -0,0 +1,24 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeFile="FutureTripsRider.aspx.cs" Inherits="FutureTripsRider" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div>
+    
+        <asp:GridView ID="GridView1" runat="server"  OnRowCommand="FutureTrips_RowCommand">
+            <Columns>
+                <asp:ButtonField ButtonType="Button" CommandName="Select" Text="End/Rate" />
+            </Columns>
+        </asp:GridView>
+        <br />
+        <br />
+    
+    </div>
+    </form>
+</body>
+</html>
---
+++ Taxim/FutureTripsRider.aspx.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+public partial class FutureTripsRider : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        SqlConClass so = new SqlConClass();
+        GridView1.DataSource = so.FilterMergedTrips(Session["E_Mail"].ToString());
+        GridView1.DataBind();
+    }
+
+    protected void FutureTrips_RowCommand(object sender,
+GridViewCommandEventArgs e)
+    {
+        if (e.CommandName == "Select")
+        {
+            // Retrieve the row index stored in the 
+            // CommandArgument property.
+            int index = Convert.ToInt32(e.CommandArgument);
+            SqlConClass so = new SqlConClass();
+            // Retrieve the row that contains the button 
+            // from the Rows collection.
+            GridViewRow row = GridView1.Rows[index];
+
+            int mId = Int32.Parse(row.Cells[1].Text);
+            Session["RatedMTID"] = mId;
+            Response.Redirect("RateDriver.aspx");
+
+        }
+
+    }
+}
